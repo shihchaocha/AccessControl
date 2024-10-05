30acf224a0f977fa821d31bc402336bdb17a9daf
csc <csc@cs.ntust.edu.tw>
2024-10-05 23:35:17 +0800

    update PAP

---
+++ PAP/src/main/java/org/example/pap/PapApplication.java
@@ -0,0 +1,13 @@
+package org.example.pap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapApplication.class, args);
+    }
+
+}
---
+++ PAP/src/test/java/org/example/pap/PapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
